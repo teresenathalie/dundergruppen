2d91b1728693557ef2f093749abf95673e3fd0c5
Nathalie <terese.nathalie@gmail.com>
2021-05-18 19:14:42 +0200

    tried someting

--- boilerplate/src/index.ts
+++ boilerplate/src/index.ts
@@ -1,1 +1,19 @@
-console.log("hello world!");
+import { Timer } from 'easytimer.js';
+import * as timerDigital from './timerDigital';
+
+let timer: Timer = new Timer();
+
+// On click start timer
+timer.start({countdown: true, startValues: {seconds: 60}})
+
+let appEl: any = document.querySelector('#app');
+
+timer.on('secondsUpdated', () => {
+    appEl.innerHTML = '';
+    appEl.insertAdjacentHTML('beforeend', timerDigital.render(timer));
+})
+
+timer.on('targetAchieved', () => {
+    appEl.innerHTML = '';
+    appEl.insertAdjacentHTML('beforeend', timerDigital.render(timer));
+})
---
+++ boilerplate/src/loading.ts
@@ -0,0 +1,11 @@
+function render() :any {
+    let template:string = `
+    <section id="loading-page">
+    <p>TEST</p>
+    </section>
+    `;
+    return template;
+}
+
+export { render }
+
