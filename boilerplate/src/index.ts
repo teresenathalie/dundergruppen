1c49b0cc2677cec45a1ff2f80aba401f63c3034e
Nathalie <terese.nathalie@gmail.com>
2021-05-18 19:31:21 +0200

    added section for loading page in index.html

--- boilerplate/src/index.ts
+++ boilerplate/src/index.ts
@@ -4,7 +4,7 @@ import * as timerDigital from './timerDigital';
 let timer: Timer = new Timer();
 
 // On click start timer
-timer.start({countdown: true, startValues: {seconds: 60}})
+timer.start({countdown: true, startValues: {seconds: 300}})
 
 let appEl: any = document.querySelector('#app');
 
