9ba2999a3977836c66f32012fa6dbee1fa80c63e
skinksteek <linusigelstrom@gmail.com>
2021-05-19 19:29:42 +0200

    added global timer, html boiler

--- boilerplate/src/index.ts
+++ boilerplate/src/index.ts
@@ -1,1 +1,19 @@
-console.log("hello world!");
+import { Timer } from 'easytimer.js';
+import * as Ticking from './modules/Ticking';
+
+let timer: Timer = new Timer();
+
+// On click start timer
+timer.start({countdown: true, startValues: {seconds: 5}})
+
+let appEl: any = document.querySelector('#app');
+
+timer.on('secondsUpdated', () => {
+    appEl.innerHTML = '';
+    appEl.insertAdjacentHTML('beforeend', Ticking.render(timer));
+})
+
+timer.on('targetAchieved', () => {
+    appEl.innerHTML = 'Times up';
+    appEl.insertAdjacentHTML('beforeend', Ticking.render(timer));
+})
